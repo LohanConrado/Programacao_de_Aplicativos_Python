03587c7ffc819003bb4a4cfda183c74e7248b477
Lohan Conrado <lohan_66@hotmail.com>
2022-03-02 12:24:59 -0300

    Update problema03.py

--- problema03.py
+++ problema03.py
@@ -2,16 +2,18 @@ valores = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 i = 0
 x = 0
 y = 0
-maior = 0
-menor = 30
 soma = 0
 media = 0
 print("Digite 20 valores!")
 
+maior = max(valores)
+menor = min(valores)
+
+
 while (i < 20):
     valores[i] = (int(input("Valor ")))
     i+=1
-
+"""
 while (x < 20):
     if maior < valores[x]:
         maior = valores[x]
@@ -26,7 +28,10 @@ while (y < 20):
     else:
         y+=1
 
-soma = sum(valores[0:20])
+"""
+maior = max(valores)
+menor = min(valores)
+soma = sum(valores)
 media = soma / i
 
-print("Média: ","\n",media,"\n","--------------------","\n","Maior numero: ", "\n",maior,"\n","--------------------","\n"," Menor numero: ","\n",menor,"\n","--------------------")
+print("\n","Média: ","\n",media,"\n","--------------------","\n","Maior numero: ", "\n",maior,"\n","--------------------","\n"," Menor numero: ","\n",menor,"\n","--------------------")
